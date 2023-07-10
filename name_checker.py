9c77536b356a8908843c5412749093a703325edc
Ethan Lee <elee@xpanxion.com>
2023-07-10 10:23:38 -0400

    Finish quiz3

--- name_checker.py
+++ name_checker.py
@@ -1,9 +1,19 @@
 from ValidationException import ValidationException
+import csv
 
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    row_list = []
+    with open(file_path) as csv_file:
+        # skip the first row
+        next(csv_file)
+        rows = csv.reader(csv_file, delimiter=',')
+        for row in rows:
+            row_list.append(row)
+    for row in row_list:
+        if any(char.isdigit() for char in row[0]):
+            raise ValidationException(f"Invalid first name: {row[0]}" )
 
-
+            
 def test():
     try:
         validate_file("users.txt")
